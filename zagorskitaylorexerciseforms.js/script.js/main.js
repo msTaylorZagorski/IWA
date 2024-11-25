965b11a35c626b24f68c070ce9e82b70a06c7129
zzzz <zzzz....@getoffmycomputer.local>
2024-11-24 18:20:13 -0800

    first commit

---
+++ zagorskitaylorexerciseforms.js/script.js/main.js
@@ -0,0 +1,13 @@
+function smoothScroll() {
+  const navLinks = document.querySelectorAll("nav a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const navHash = document.querySelector(link.hash);
+      navHash.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+}
+
+smoothScroll();
+<script src="script.js"></script>;
